f07649a7f130e27df76b6adfda1ebdd670c771e8
GruntBucket <simonsky42@gmail.com>
2016-12-02 18:22:32 -0800

    Add project files.

---
+++ BMOTD/Program.cs
@@ -0,0 +1,18 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMOTD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyBot bot = new MyBot();
+
+        }
+    }
+}
